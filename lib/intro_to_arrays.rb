51c8774579b1dd314dc2ad1ec06cf267af1d0e7d
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-10 15:39:36 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  ["a", "b"]
+end
+
+def first_element(sample_array)
+  sample_array[0]
+end
+
+def third_element(sample_array)
+  sample_array[2]
+end
+
+def last_element(sample_array)
+  sample_array[-1]
+end
+
+def first_element_with_array_methods(sample_array)
+  sample_array.first
+end
+
+def last_element_with_array_methods(sample_array)
+  sample_array.last
+end
+
+def length_of_array(sample_array)
+  sample_array.length
+end
